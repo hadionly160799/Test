7f9028b294b2c646203490537e3361bed6c3fef7
hadionly160799 <34292269+hadionly160799@users.noreply.github.com>
2022-04-15 16:23:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -14,9 +14,9 @@ driver.find_element_by_name("email").send_keys(email)
 driver.find_element_by_name("password").send_keys(password)
 driver.find_element_by_id("login-button").click()
 
-
-
-my_mage = WebDriverWait(driver, 30).until(EC.url_contains("?code"))
+element = WebDriverWait(driver, 30).until(EC.presence_of_element_located(('id', 'duerr-dental-logo')))
+driver.get(url)
+#print("Yes have")
 
 
 
