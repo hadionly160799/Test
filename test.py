3f79098380baab1ad42bf9e30b9e76c86baed248
hadionly160799 <34292269+hadionly160799@users.noreply.github.com>
2022-04-15 16:09:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 email = "dd_test_1@outlook.com"
 password = "}krK,gdC6"
@@ -8,9 +10,19 @@ url = "https://vsmonitor.com"
 driver = webdriver.Chrome("C:\webdriver1\chromedriver.exe")
 driver.get(url)
 driver.implicitly_wait(15)
-
-
 driver.find_element_by_name("email").send_keys(email)
 driver.find_element_by_name("password").send_keys(password)
 driver.find_element_by_id("login-button").click()
+
+
+
+my_mage = WebDriverWait(driver, 30).until(EC.url_contains("?code"))
+
+
+
+
+
+
+
+
 
